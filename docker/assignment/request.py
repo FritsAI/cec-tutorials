95781b5742f523eda0c29be6232cf0a260f0e7a2
FritsvanH <71830009+FritsvanH@users.noreply.github.com>
2024-09-18 19:59:46 +0200

    new_test

---
+++ docker/assignment/request.py
@@ -0,0 +1,20 @@
+import requests
+
+notification_service_url = "http://localhost:3000/api/notify"
+
+data = {
+    "notification_type": "OutOfRange", 
+    "researcher": "d.landau@uu.nl",
+    "measurement_id": "1234", 
+    "experiment_id": "5678", 
+    "cipher_data": "D5qnEHeIrTYmLwYX.hSZNb3xxQ9MtGhRP7E52yv2seWo4tUxYe28ATJVHUi0J++SFyfq5LQc0sTmiS4ILiM0/YsPHgp5fQKuRuuHLSyLA1WR9YIRS6nYrokZ68u4OLC4j26JW/QpiGmAydGKPIvV2ImD8t1NOUrejbnp/cmbMDUKO1hbXGPfD7oTvvk6JQVBAxSPVB96jDv7C4sGTmuEDZPoIpojcTBFP2xA"
+}
+
+#request_response = requests.post(url=notification_service_url, data=data)
+response = requests.get(notification_service_url,data)
+
+#print(request_response.text)
+print(response.text)
+#open with(,'a+')
+
+
--- docker/assignment/run.sh
+++ docker/assignment/run.sh
@@ -3,5 +3,6 @@
 docker run \
     --rm -d \
     --name notifications-service \
+    --network lab1network \
     -p 3000:3000 \
     dclandau/cec-notifications-service "$@"
